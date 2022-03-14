0f5fa10bddd8d4ddb6f55a176a87e1a03d290beb
Champen <64540361+Champen@users.noreply.github.com>
2022-03-14 09:05:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+
+
+var words = ['Lars.','a computer scientist.', 'a web developer.', ' a web designer.'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
+
+//------------Skills Section----------//
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function() {
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length > 0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
+
+
+//------------Projects Section----------//
+
+
+$('.portfolio-menu ul li').click(function() {
+  $('.portfolio-menu ul li').removeClass('active');
+  $(this).addClass('active');
+
+  var selector = $(this).attr('data-filter');
+  $('.portfolio-item').isotope({
+    filter: selector
+  });
+  return false;
+});
+$(document).ready(function() {
+  var popup_btn = $('.popup-btn');
+  popup_btn.magnificPopup({
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+});
